cdd589544e39544a9073501e290da0329ccb78e2
Zainul Franciscus <Zainul.Franciscus@myob.com>
2018-12-24 12:02:56 +1100

    add validation into the form.

--- src/components/form.js
+++ src/components/form.js
@@ -1,14 +1,28 @@
 import React from 'react';
 import {Field, reduxForm} from 'redux-form';
 
+const renderField = ({
+                         input,
+                         label,
+                         type,
+                         meta: {touched, error, warning}
+                     }) => (
+    <span>
+        <input {...input} placeholder={label} type={type}/>
+        {touched &&
+        ((error && <span>{error}</span>) ||
+            (warning && <span>{warning}</span>))}
+    </span>
+);
+
 let CalculatorForm = props => {
     const {handleSubmit} = props;
     return (
         <div>
             <form onSubmit={handleSubmit}>
-                <Field name="firstNumber" component="input" type="text"/>
+                <Field name="firstNumber" component={renderField} type="text"/>
                 <button type="submit">+</button>
-                <Field name="secondNumber" component="input" type="text"/>
+                <Field name="secondNumber" component={renderField} type="text"/>
                 =
                 <Field name="result" component="label" type="label"/>
                 <label id="result">0</label>
@@ -17,8 +31,19 @@ let CalculatorForm = props => {
     )
 };
 
-CalculatorForm = reduxForm({
-    form: 'calculator'
-})(CalculatorForm);
+const validate = values => {
+    const errors = {};
 
-export default CalculatorForm;
+    Object.entries(values).forEach((value) => {
+        if(isNaN(value[1])){
+            errors[value[0]] = 'must be a number';
+        }
+    });
+
+    return errors;
+};
+
+export default reduxForm({
+    form: 'calculator',
+    validate
+})(CalculatorForm);
